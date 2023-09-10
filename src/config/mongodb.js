5c8dc0e2c09d6508b55dd8d916f5431f272bd1ec
duynghiadev <duynghia22302@gmail.com>
2023-09-10 23:41:26 +0700

    #46. Điểm qua nhanh MongoDB Compass và NoSQL Booster | NodeJS + MongoDB | TrungQuanDev

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,5 @@
 
 const MONGODB_URI =
   'mongodb+srv://duynghiadev:bsPX2D2uCbIRP0x9@cluster0-trungquandev.ygftgpu.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'trello-trungquandev-mern-stack-pro'
