3c69963e28c8aca4fae37f87bb76d9c913c7c7f7
duynghiadev <duynghia22302@gmail.com>
2023-09-17 17:59:20 +0700

    47. Tối ưu kết nối MongoDB Atlas vào dự án Back-end | NodeJS + MongoDB | TrungQuanDev

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,7 +1,36 @@
-// duynghiadev
-// bsPX2D2uCbIRP0x9
-
 const MONGODB_URI =
   'mongodb+srv://duynghiadev:bsPX2D2uCbIRP0x9@cluster0-trungquandev.ygftgpu.mongodb.net/?retryWrites=true&w=majority'
 
 const DATABASE_NAME = 'trello-trungquandev-mern-stack-pro'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+// Khởi tạo một đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
+let trelloDatabaseInstance = null
+
+// Khởi tạo một đối tượng mongoClientInstance để connect tới MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  // Lưu ý: cái serverApi có từ phiên bản MongoDB 5.0.0 trở lên, có thể không cần dùng nó, còn nếu dùng nó là chúng ta sẽ chỉ định một cái Stable API Version của MongoDB
+  // Đọc thêm ở đây: https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Kết nối tới Database
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên của chúng ta
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB (không async) này có nhiệm vụ export ra cái Trello Database Instance sau khi đã connect thành công tới MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code
+// Lưu ý phải đảm bảo chỉ luôn gọi cái GET_DB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
