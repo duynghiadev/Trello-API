50946e750a3d3162a7be245cf75999d8479eb519
duynghiadev <duynghia22302@gmail.com>
2024-02-09 17:13:16 +0700

    62. CORS đâu có lỗi lầm gì? | Rất nhiều bạn đang hiểu nhầm về CORS | NodeJS + MongoDB | TrungQuanDev

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+// Những domain được phép truy cập tới tài nguyên của Server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // Ví dụ sau này sẽ deploy lên domain chính thức thì phải thêm domain vào back-end sau khi chúng ta deploy, thì back-end mới cho truy cập domain
+]
