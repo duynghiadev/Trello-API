7824aab2a3b106ddd4563bd1958d8dcacada9787
duynghiadev <duynghia22302@gmail.com>
2024-02-10 21:29:48 +0700

    70. Hoàn thiện tích hợp kéo thả Columns với API | ReactJS, NodeJS, MongoDB | TrungQuanDev

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -22,6 +22,9 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+// Chỉ định ra những Fields mà chúng ta không muốn cho phép cập nhật trong hàm update()
+const INVALID_UPDATE_FIELDS = ['_id', 'createdAt']
+
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
     abortEarly: false
@@ -109,10 +112,38 @@ const pushColumnOrderIds = async (column) => {
           }
         },
         {
-          returnDocument: 'after'
+          returnDocument: 'after' // Sẽ trả về kết quả mới sau khi cập nhật
         }
       )
-    return result.value
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (boardId, updateData) => {
+  try {
+    // Lọc những field mà chúng ta không cho phép cập nhật linh tinh
+    Object.keys(updateData).forEach((fieldName) => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOneAndUpdate(
+        {
+          _id: new ObjectId(boardId)
+        },
+        {
+          $set: updateData
+        },
+        {
+          returnDocument: 'after' // Sẽ trả về kết quả mới sau khi cập nhật
+        }
+      )
+    return result
   } catch (error) {
     throw new Error(error)
   }
@@ -124,5 +155,6 @@ export const boardModel = {
   createNew,
   findOneById,
   getDetails,
-  pushColumnOrderIds
+  pushColumnOrderIds,
+  update
 }
